f7796ad17825b3a13cddefa306851723c088a01e
Karim_Gamal <81161929+KarimGamal97@users.noreply.github.com>
2024-09-17 00:21:26 +0300

    sdsddefer

--- assets/js/main.js
+++ assets/js/main.js
@@ -409,4 +409,10 @@ $(document).ready(function () {
   });
 });
 
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
 // Test
